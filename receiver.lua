43f1942ad7fe418f2b6be8769a79c325fa4f76e1
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-22 17:06:33 +0700

    uploaded main files

---
+++ receiver.lua
@@ -0,0 +1,19 @@
+rednet.wrap("top")
+
+local sx,sy,sz = gps.locate()
+
+local targets = {}
+
+while true do
+    local id,tar,x,y,z = rednet.receive()
+    targets[tar].x = x
+    targets[tar].y = y
+    targets[tar].z = z
+    local dx,dy,dz = sx-x,sy-y,sz-z
+    local d = sqrt(dx^2,dy^2,dz^2)
+    targets[tar].d = d
+    for i,v in pairs(targets) do
+        print(i.." pos: "..v.x..", "..v.y..", "..v.z)
+        print(i.." dis: "..v.d)
+    end
+end
---
+++ sender.lua
@@ -0,0 +1,19 @@
+local target = "NIL"
+
+function main()
+    while true do
+        local x,y,z = gps.locate()
+        rednet.broadcast(target,x,y,z)
+        sleep(0.1)
+    end
+end
+
+while true do
+    local s,e = pcall(main)
+
+    if not s then
+        if e == "Terminated" then
+            os.reboot()
+        end
+    end
+end
