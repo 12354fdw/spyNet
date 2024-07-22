dffb39c763ca2f0e0ee259a8f00dd7848fc7d003
12354fdw <88530369+12354fdw@users.noreply.github.com>
2024-07-22 17:20:05 +0700

    Update receiver.lua

--- receiver.lua
+++ receiver.lua
@@ -1,9 +1,13 @@
 rednet.open("top")
-
+ 
 local sx,sy,sz = gps.locate()
-
+ 
 local targets = {}
-
+ 
+term.clear()
+term.setCursorPos(1,1)
+print("-----------[spyNet]-----------")
+ 
 while true do
     local id,tar,x,y,z = rednet.receive()
     targets[tar].x = x
@@ -13,8 +17,9 @@ while true do
     local d = sqrt(dx^2,dy^2,dz^2)
     targets[tar].d = d
     for i,v in pairs(targets) do
-        term.clear()
-        term.setCursorPos(1,1)
+		term.clear()
+		term.setCursorPos(1,1)
+		print("-----------[spyNet]-----------")
         print(i.." pos: "..v.x..", "..v.y..", "..v.z)
         print(i.." dis: "..v.d)
     end
